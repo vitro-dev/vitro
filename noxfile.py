ec86c123664cce7c1ab08f06700a72c19c13d48f
Filippo Carra <karrukola@users.noreply.github.com>
2026-02-10 16:56:22 +0100

    ci(noxfile.py): extend validation to py3.14t and include lowest dep. resolution
    Signed-off-by: Filippo Carra <karrukola@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,26 @@
 """Validation sessions."""
 
+from __future__ import annotations
+
+from typing import Literal
+
 import nox
 
-_PYTHON_VERSIONS = [
+ResolutionType = Literal["highest", "lowest-direct", "lowest"]
+_PYTHON_VERSIONS = (
     "3.10",
     "3.11",
     "3.12",
     "3.13",
     "3.14",
-]
+    "3.14t",
+)
+_RESOLUTIONS: tuple[ResolutionType, ...] = (
+    "highest",
+    # "lowest-direct",
+    "lowest",
+)
+
 
 # Fail nox session when run a program which is not installed in its virtualenv
 nox.options.error_on_external_run = True
@@ -17,21 +29,23 @@ nox.options.error_on_external_run = True
 nox.options.default_venv_backend = "uv"
 
 
-def _install_deps(session: nox.Session) -> None:
+def _install_deps(session: nox.Session, resolution: ResolutionType) -> None:
     session.run_install(
         "uv",
         "sync",
         "-q",
         "--frozen",
         f"--python={session.virtualenv.location}",
+        f"--resolution={resolution}",
         env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
     )
 
 
 @nox.session(python=_PYTHON_VERSIONS)
-def lint(session: nox.Session) -> None:
+@nox.parametrize("resolution", _RESOLUTIONS)
+def lint(session: nox.Session, resolution: ResolutionType) -> None:
     """Perform style checks of the code."""
-    _install_deps(session)
+    _install_deps(session=session, resolution=resolution)
     session.run("ruff", "format", "--check", ".")
     session.run("ruff", "check", ".")
     session.run("mypy", "./src/", "./tests/")
@@ -40,28 +54,27 @@ def lint(session: nox.Session) -> None:
 @nox.session
 def coverage_clean(session: nox.Session) -> None:
     """Clean coverage data."""
-    _install_deps(session)
+    _install_deps(session=session, resolution="highest")
     session.run("coverage", "erase")
 
 
 @nox.session(python=_PYTHON_VERSIONS, requires=("coverage_clean",))
-def tests(session: nox.Session) -> None:
+@nox.parametrize("resolution", _RESOLUTIONS)
+def test(session: nox.Session, resolution: ResolutionType) -> None:
     """Execute the tests and measure code coverage."""
     # Speed up coverage by using sysmon, see
     # https://coverage.readthedocs.io/en/latest/cmd.html#execution-coverage-run
     # TODO: move to pyproject.toml once 3.12 is the min. supported version
-
-    py_ver = tuple(map(int, session.python.split(".")))  # type: ignore[union-attr]
-    extra_env = {"COVERAGE_CORE": "sysmon"} if py_ver >= (3, 12) else None
-    _install_deps(session)
+    extra_env = {"COVERAGE_CORE": "sysmon"}
+    _install_deps(session=session, resolution=resolution)
     session.run("coverage", "run", "-m", "pytest", "tests", env=extra_env)
 
-    session.notify("collate_coverage")
+    session.notify("coverage_collate")
 
 
 @nox.session
-def collate_coverage(session: nox.Session) -> None:
+def coverage_collate(session: nox.Session) -> None:
     """Combine test coverage results from all test executions."""
-    _install_deps(session)
+    _install_deps(session=session, resolution="highest")
     session.run("coverage", "combine")
     session.run("coverage", "html")
